93f2daa297b87181a386f2af5e2e469a3830b381
manucastelnovo <marnutskate@gmail.com>
2025-01-22 12:35:52 -0300

    Add font imports for Roboto and Lato in UI

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import {Lato, Inter, Roboto} from 'next/font/google';
+
+export const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
+
+export const lato = Lato({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
