d50bed2196f3a3969dcc555bbae0b224eb94d1b3
manucastelnovo <marnutskate@gmail.com>
2025-01-22 16:10:02 -0300

    Update LoginForm component styles and text for localization

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,30 +1,26 @@
-import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
-} from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+} from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { Button } from "./button";
+import { roboto } from "./fonts";
 
 export default function LoginForm() {
   return (
     <form className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+      <div className="flex-1 rounded-lg md:w-[480px] md:h-[454px] bg-purple-200 px-6 pb-4 pt-8">
+        <h1
+          className={`${roboto.className} antialiased w-full font-bold text-4xl mb-8 text-center`}
+        >
+          Iniciar Sesion
         </h1>
         <div className="w-full">
           <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full h-12 mb-4 rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="email"
                 type="email"
                 name="email"
@@ -35,15 +31,9 @@ export default function LoginForm() {
             </div>
           </div>
           <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full h-12 mb-4 rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
                 type="password"
                 name="password"
@@ -55,11 +45,18 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full">
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
+        <Button className="mt-4 w-full">Iniciar Sesión</Button>
+        <hr className="mt-8 border-secundaryGray" />
         <div className="flex h-8 items-end space-x-1">
           {/* Add form errors here */}
+        </div>
+        <div className="mt-4 text-center">
+          <a
+            href="/forgot-password"
+            className="text-sm text-black hover:underline"
+          >
+            ¿Olvidó su contraseña?
+          </a>
         </div>
       </div>
     </form>
