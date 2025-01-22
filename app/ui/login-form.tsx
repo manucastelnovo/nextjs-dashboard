ad84a6118b29ab645e676eadff9f44e37aa2dc5e
manucastelnovo <marnutskate@gmail.com>
2025-01-22 16:28:58 -0300

    Update LoginForm component styles for improved layout and localization

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -10,47 +10,45 @@ import { roboto } from "./fonts";
 export default function LoginForm() {
   return (
     <form className="space-y-3">
-      <div className="flex-1 rounded-lg md:w-[480px] md:h-[454px] bg-purple-200 px-6 pb-4 pt-8">
+      <div className="flex-1 rounded-lg md:w-[480px] md:h-[454px] bg-purple-200 px-12 pb-4 pt-12">
         <h1
-          className={`${roboto.className} antialiased w-full font-bold text-4xl mb-8 text-center`}
+          className={`${roboto.className} antialiased w-full font-bold text-5xl mb-8 text-center`}
         >
           Iniciar Sesion
         </h1>
-        <div className="w-full">
+        <div className={`w-full ${roboto.className} antialiased`}>
           <div>
             <div className="relative">
               <input
-                className="peer block w-full h-12 mb-4 rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full h-12 mb-4 rounded-md border border-gray-200 py-[9px] pl-2 text-sm outline-2 placeholder:text-gray-500"
                 id="email"
                 type="email"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="Email"
                 required
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           <div className="mt-4">
             <div className="relative">
               <input
-                className="peer block w-full h-12 mb-4 rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full h-12 mb-4 rounded-md border border-gray-200 py-[9px] pl-2 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
                 type="password"
                 name="password"
-                placeholder="Enter password"
+                placeholder="Contraseña"
                 required
                 minLength={6}
               />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full">Iniciar Sesión</Button>
-        <hr className="mt-8 border-secundaryGray" />
-        <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
-        </div>
-        <div className="mt-4 text-center">
+        <Button className="mt-4 w-full h-14 text-xl">Iniciar Sesión</Button>
+        <hr className="mt-8 mb-10 border-secundaryGray" />
+        {/* <div className="flex h-8 items-end space-x-1"> */}
+        {/* Add form errors here */}
+        {/* </div> */}
+        <div className="text-center">
           <a
             href="/forgot-password"
             className="text-sm text-black hover:underline"
